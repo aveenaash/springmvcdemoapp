1b530df44f068f5e06c2ba73bcd030453d4a7aac
Abinash <abinash@Abinashs-MacBook-Pro.local>
2020-02-19 17:18:15 -0500

    request param added

--- src/main/java/abc/xy/models/Customer.java
+++ src/main/java/abc/xy/models/Customer.java
@@ -2,8 +2,17 @@ package abc.xy.models;
 
 public class Customer {
 
+	private String id;
 	private String name;
 	private String email;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
 
 	public String getName() {
 		return name;
---
+++ src/main/webapp/pages/customer.jsp
@@ -0,0 +1,17 @@
+<html>
+<body>
+<h2>Customer Home Page</h2>
+
+<h1>Welcome ${customer.name}</h1>
+<table>
+	<tr>
+		<td>Id : </td>
+		<td>${customer.id}</td>
+	</tr>
+	<tr>
+		<td>Email : </td>
+		<td>${customer.email}</td>
+	</tr>
+</table>
+</body>
+</html>
